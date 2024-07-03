5dec8b5cf2ca9f12b93077e22b09fc6882c803ff
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-07-03 13:27:06 +0400

    Added TMDB movies API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import cards_data from "../../config/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  console.log(category);
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2FjMDJmNzk2NDQ3NjJlZWM1NzMyZGUxMzE3ZjIwMSIsIm5iZiI6MTcxOTk5MzEyNi41MjkzMzgsInN1YiI6IjY1M2E0ZTE5MjgxMWExMDE0ZDYxMWY5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cWFkbFYZChcdre0QxHVr09Ke98CzDuMVriHwNpiGom8",
+    },
+  };
 
   const handlWheel = (event) => {
     event.preventDefault();
@@ -11,6 +21,16 @@ const TitleCards = ({ title, category }) => {
   };
 
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handlWheel);
   }, []);
 
@@ -18,11 +38,14 @@ const TitleCards = ({ title, category }) => {
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt={card.name} />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`}
+                alt={card.original_title}
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
